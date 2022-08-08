1b24b809bad9fb529572fa753f144dc3ef6037bc
Luiz Henrique <luizhenrique92sp@gmail.com>
2022-08-08 15:36:36 -0300

    Projeto finalizado, no futuro modificar de CSS para SCSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+import {pecas} from "./estatisticas.js";
+
 const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatisticas]")
 
 controle.forEach( (elemento) => {
    elemento.addEventListener("click", (evento) => {
       manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+      atualizaEstatisticas(evento.target.dataset.controle, evento.target.dataset.peca)
    }) 
 })
 
@@ -12,5 +16,17 @@ function manipulaDados(operacao, controle) {
       peca.value = parseInt(peca.value) - 1
    } else {
       peca.value = parseInt(peca.value) + 1
+   }
+}
+
+function atualizaEstatisticas(operacao, peca) {
+   if (operacao === "+") {
+      estatisticas.forEach((elemento) => {
+         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatisticas]
+      })
+   } else {
+      estatisticas.forEach((elemento) => {
+         elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatisticas]
+      })
    }
 }
